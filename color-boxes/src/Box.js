f953de44a9eea1e493ce12f1cac7bca34ac3dd17
huangfuin11 <huangfuin1101@yahoo.com.tw>
2020-06-02 20:04:13 +0800

    created Box component

---
+++ color-boxes/src/Box.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import "./Box.css";
+
+class Box extends Component {
+  constructor(props){
+    super(props);
+    this.state = {color: 'purple'};
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+  }
+  render() {
+    return (
+      <div>
+        <div className='Box'
+        style={{ backgroundColor: this.state.color }}
+        onClick={this.handleClick}
+      />
+        
+      </div>
+    )
+  }
+}
+
+export default Box;
+
